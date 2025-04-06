eb14bbd686c5fbf2c9737ebe396224617b3d3deb
varadha17 <abinash.varadharajan@gmail.com>
2025-04-06 18:53:34 +0530

    [search] changes in playwright yml

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['line'],
-    ["allure-playwright"]
+    ['allure-playwright']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
